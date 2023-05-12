924f3be120146cc6f09fa1b44715e7fef85fa3c4
breaker19 <sebakarp26@gmail.com>
2023-05-11 22:31:07 -0300

    update pass

--- config/mongoDb.config.js
+++ config/mongoDb.config.js
@@ -1,1 +1,1 @@
-export const MONGODB_CNX_STR= "mongodb+srv://sebakarp26:floresycolores@test.ctqc9tr.mongodb.net/Eccommerce?retryWrites=true&w=majority";
+export const MONGODB_CNX_STR= "mongodb+srv://sebakarp26:xxxx@test.ctqc9tr.mongodb.net/Eccommerce?retryWrites=true&w=majority";
